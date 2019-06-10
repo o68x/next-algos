abbedc9e7c8b2fea93301bd6887ea08d857691b7
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-06-10 17:31:07 +0200

    fun with sums problem

---
+++ pb-1/sums.rb
@@ -0,0 +1,30 @@
+# sums
+
+list = *(1..100)
+
+def sum_while(list)
+  i = 0
+  sum = 0
+  while i < list.count do
+    sum += list[i]
+    i += 1
+  end
+  return sum
+end
+
+def sum_each(list)
+  sum = 0
+  list.each { |i| sum += i}
+  return sum
+end
+
+def sum_recursive(list)
+  list = [list.slice!(0) + list.slice!(0)] + list
+  return list[0] if list.count == 1
+  sum_recursive(list)
+end
+
+puts "'Ruby' sum:      #{list.sum}"
+puts "'While' sum:     #{sum_while(list)}"
+puts "'Each' sum:      #{sum_each(list)}"
+puts "'Recursive' sum: #{sum_recursive(list)}"
