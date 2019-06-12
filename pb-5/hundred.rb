3c97b918ed66c1417668e6ca5e9410e4c7ef8398
Olivier Cardinaux <ocardinaux@gmail.com>
2019-06-12 22:53:07 +0200

    Create hundred.rb

---
+++ pb-5/hundred.rb
@@ -0,0 +1,15 @@
+# 1 2 3 4 5 6 7 8 9
+#
+# each pair has 4 possibilities -1, 1, sum, join
+# ex: [2, 3] gives [-1, 1, 5, 23]... and so on
+#
+# how do we proceed?
+
+1, 2 -> -1 1 3 12
+2, 3 ->     5 23
+3, 4
+4, 5
+5, 6
+6, 7
+7, 8
+8, 9
